7514b4e517c66c641a0115a1d47139ef1f79437b
Andrespolicrack <andrespoli071009@gmail.com>
2024-02-16 18:42:18 +0100

    Upgrade

--- lib/classes/Slash.ts
+++ lib/classes/Slash.ts
@@ -4,7 +4,7 @@ import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 import * as colors from 'colors';
 
-export async function Subir(path: string, token: string) {
+export async function Subir(path: string, token: string, clientId: string) {
     const commands = new Array();
 
     fs.readdirSync(path + "/").forEach((dir) => {
@@ -15,12 +15,30 @@ export async function Subir(path: string, token: string) {
                 if (slash && slash.data) {
                     commands.push(slash.data.toJSON());
                 } else {
-
+                    const date = new Date();
+                    throw new Error(colors.cyan(`[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]`) + ' ' + colors.red(__dirname) + colors.red(` Data is not defined on ${file}`))
                 }
             } catch (err) {
                 const date = new Date();
-                console.error(colors.cyan(`[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]`) + ' ' + colors.red(__dirname) + colors.red(` I got an error pushing ${file}`))
+                throw new Error(colors.cyan(`[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]`) + ' ' + colors.red(__dirname) + colors.red(` I got an error pushing ${file}`))
             }
         }
     });
+    const rest = new REST({ version: "9" }).setToken(token);
+
+    createSlash();
+
+    async function createSlash() {
+        try {
+            await rest.put(
+                Routes.applicationCommands(clientId), {
+                    body: commands
+                }
+            )
+            const date = new Date();
+            console.log(colors.cyan(`[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]`) + ' ' + colors.green(__dirname) + colors.green(' Slash Commands Uploaded'))
+        } catch (err) {
+            throw new Error(`${err}`)
+        }
+    }
 }
